1fc443b22f84b8cfba888a3723d770a6b0b4be07
seungkwon <yevgnenll@gmail.com>
2016-03-28 16:07:53 +0900

    Post model design
    it almost same instagram
    so it has 5 column
    content, image, create_at, updated_at, user

---
+++ fastagram/posts/models/post.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from users.models import User
+
+
+class Post(models.Model):
+
+    content = models.TextField()
+
+    image = models.ImageField(
+        blank=True,
+        null=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_add_now=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    user = models.ForeignKey(
+        User,
+    )
