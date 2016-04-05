98d6fedff6bffc7265c5a350ba76325cad0b2579
seungkwon <yevgnenll@gmail.com>
2016-04-05 17:38:05 +0900

    signup validation check more concrete
    when check password confirm
    if incorrect each password
    form will not submit

--- fastagram/fastagram/static/js/signup.js
+++ fastagram/fastagram/static/js/signup.js
@@ -1,29 +1,22 @@
 (function(){
-
-    var is_check = true;
-
+    
     $(document).ready(function(){
+
         var my_pass = $('#firstpw');
         var check_pw = $('#secondpw');
         var chk_label = $('#checked');
+        var signup = $('#signup');
 
         check_pw.keyup(function(){
             if (my_pass.val() === check_pw.val()) {
-                chk_label.text('correct password').removeClass('check_user_data');
-                is_check = false; 
+                chk_label.text('correct password');                
+                signup.prop('disabled', false);
             } else {
-                chk_label.text('invalid password').addClass('check_user_data');
-                is_check = true;
+                chk_label.text('invalid password').css('font-size', '20px').css('color','#ff1744');
+
+                signup.prop('disabled', true);
             }
         });
     });
 
-    $(document).ready(function(){
-        $('#signup').click(function(){
-            if(is_check){
-                $('#signup').disabled = true;
-            }
-        });
-        
-    });
 })();
